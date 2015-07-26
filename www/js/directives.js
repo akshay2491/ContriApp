7e939f33d263b5e351ef43f14338dffb1767a018
Akshay <akshay.kalekar007@gmail.com>
2015-07-26 09:19:42 +0530

    loading if image is not loaded done

--- www/js/directives.js
+++ www/js/directives.js
@@ -11,4 +11,36 @@ angular.module('starter.directives',[])
             scope.$apply(attrs['myclick']);
         });
     };
+})
+.directive('threshold', function($timeout) {
+  var loadSuccess = {};
+  return {
+    link: function(scope, element, attrs) {
+      element.bind('error', function() {
+        scope.$apply(function() {
+          element.attr('src', element.attr('data-src-fallback'));
+        });
+      });
+      element.bind('load', function() {
+        loadSuccess[element.attr('ng-src')] = true;
+        element.attr('src',element.attr('ng-src'));
+      });
+      
+      var timeout = -1;
+      try {
+        var timeout = parseInt(element.attr('data-src-maxtime'), 10);
+        loadSuccess[element.attr('ng-src')] =false;
+      } catch (ex) {
+      }
+      if (timeout != -1) {
+        if (!loadSuccess[element.attr('ng-src')]) {
+          $timeout(function() {
+            if (!loadSuccess[element.attr('ng-src')]) {
+              element.attr('src', element.attr('data-src-fallback'));
+            }
+          }, timeout);
+        }
+      }
+    }
+  };
 });
